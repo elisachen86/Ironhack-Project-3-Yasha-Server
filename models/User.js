818de4526e4409cae2d1f4f449bd3f25039f104a
Elisa Chen <skechen@gmail.com>
2021-01-13 17:24:06 +0100

    edits to user model|

--- models/User.js
+++ models/User.js
@@ -2,8 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  firstName: String,
+  lastName: String,
   email: { type: String, required: true },
   password: { type: String, required: true },
+  avatar: String,
+  permission: { type: String, enum: ["admin", "editor"] },
+  userType: { type: String, enum: ["retailer", "brand"] },
+  company: { type: Schema.Types.ObjectId, ref: "company" },
 });
 
 const User = mongoose.model("User", userSchema);
