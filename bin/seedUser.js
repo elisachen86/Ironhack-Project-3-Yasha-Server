cd3d40d3b8e4d4c43143d955d7cb8d00734bf60d
Elisa Chen <skechen@gmail.com>
2021-01-16 11:04:51 +0100

    changed default user picture so more visible

--- bin/seedUser.js
+++ bin/seedUser.js
@@ -1,101 +1,96 @@
-require("dotenv").config()
-require("./../config/dbConnection")
-const User = require("./../models/User")
+require("dotenv").config();
+require("./../config/dbConnection");
+const User = require("./../models/User");
 const bcrypt = require("bcrypt");
 
 const salt = 10;
 
-const users =  [
-    {
+const users = [
+  {
     firstName: "Elisa",
-    lastName: "Chen", 
-    email:  "elisachenririiiiii@gmail.com",
+    lastName: "Chen",
+    email: "elisachenririiiiii@gmail.com",
     password: "toto",
     permission: "admin",
     userType: "retailer",
-},
-{
+  },
+  {
     firstName: "Marine",
-    lastName: "Sanjuan", 
-    email:  "pamplemarine@gmail.com",
+    lastName: "Sanjuan",
+    email: "pamplemarine@gmail.com",
     password: "toto",
     permission: "editor",
     userType: "retailer",
-},
+  },
 
-{
+  {
     firstName: "Yu",
-    lastName: "Miao", 
-    email:  "yuuu.miao@gmail.com",
+    lastName: "Miao",
+    email: "yuuu.miao@gmail.com",
     password: "toto",
     permission: "editor",
     userType: "brand",
-},
+  },
 
-{
-      firstName: "Frank",
-      lastName: "M", 
-      email:  "frank@youtuber.com",
-      password: "toto",
-      permission: "editor",
-      userType: "retailer",
-},
+  {
+    firstName: "Frank",
+    lastName: "M",
+    email: "frank@youtuber.com",
+    password: "toto",
+    permission: "editor",
+    userType: "retailer",
+  },
 
-{
-      firstName: "Andy",
-      lastName: "T", 
-      email:  "andy@hacker.com",
-      password: "toto",
-      permission: "editor",
-      userType: "retailer",
-},
+  {
+    firstName: "Andy",
+    lastName: "T",
+    email: "andy@hacker.com",
+    password: "toto",
+    permission: "editor",
+    userType: "retailer",
+  },
 
-{
-      firstName: "Vincent",
-      lastName: "P", 
-      email:  "vincent@911.com",
-      password: "toto",
-      permission: "editor",
-      userType: "brand",
-},
+  {
+    firstName: "Vincent",
+    lastName: "P",
+    email: "vincent@911.com",
+    password: "toto",
+    permission: "editor",
+    userType: "brand",
+  },
 
+  {
+    firstName: "Fanny",
+    lastName: "Sabin",
+    email: "fsabin@iron.com",
+    password: "toto",
+    permission: "editor",
+    userType: "brand",
+  },
+  {
+    firstName: "titi",
+    lastName: "titi",
+    email: "titi@titi.com",
+    password: "toto",
+    permission: "editor",
+    userType: "retailer",
+  },
 
-{
-      firstName: "Fanny",
-      lastName: "Sabin", 
-      email:  "fsabin@iron.com",
-      password: "toto",
-      permission: "editor",
-      userType: "brand",
-},
-{
-      firstName: "titi",
-      lastName: "titi", 
-      email:  "titi@titi.com",
-      password: "toto",
-      permission: "editor",
-      userType: "retailer",
-},
-
-{
-      firstName: "toto",
-      lastName: "toto", 
-      email:  "tototo@toto.com",
-      password: "toto",
-      permission: "editor",
-      userType: "brand",
-},
-
-]
-
+  {
+    firstName: "toto",
+    lastName: "toto",
+    email: "tototo@toto.com",
+    password: "toto",
+    permission: "editor",
+    userType: "brand",
+  },
+];
 
 users.forEach((oneUser) => {
-
   const hashedPassword = bcrypt.hashSync(oneUser.password, salt);
   // console.log(hashedPassword)
-  oneUser.password = hashedPassword 
-  
-})
+  oneUser.password = hashedPassword;
+});
 
 User.deleteMany()
   .then(async () => {
