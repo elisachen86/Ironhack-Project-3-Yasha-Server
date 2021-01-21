8f6ab9c5ed821c63ed7108b5830b8c5b2a6f2666
Elisa Chen <skechen@gmail.com>
2021-01-21 11:13:09 +0100

    added public folder through reacct build script

---
+++ public/precache-manifest.227ec50db34459520520c717fdc7763e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9083d2e6fb83277f4cbccccbfc4361",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7cec09a9167f439166e",
+    "url": "/static/css/main.1b50b75a.chunk.css"
+  },
+  {
+    "revision": "fefeabe2970553aa0ec6",
+    "url": "/static/js/2.cf20c571.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.cf20c571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7cec09a9167f439166e",
+    "url": "/static/js/main.7274b525.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
