3f278c0c35cf1ec6610b946e007a81140b946122
Elisa Chen <skechen@gmail.com>
2021-01-21 11:53:24 +0100

    fixing deploy bugs

--- app.js
+++ app.js
@@ -52,6 +52,13 @@ app.use("/api/user", userRouter);
 app.use("/api/company", companyRouter);
 app.use("/api/order", orderRouter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use("*", (req, res, next) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(__dirname + "/public/index.html");
+  });
+}
+
 // 404 Middleware
 app.use((req, res, next) => {
   const error = new Error("Ressource not found.");
@@ -67,6 +74,7 @@ app.use((err, req, res, next) => {
   if (process.env.NODE_ENV !== "production") {
     console.error(err);
   }
+  console.log(err);
   console.log("An error occured");
   res.status(err.status || 500);
   if (!res.headersSent) {
